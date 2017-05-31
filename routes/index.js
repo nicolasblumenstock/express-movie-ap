dd92bd4949c48fecd07e05237034f32df68ab665
Nicolas Blumenstock <nicpblum@gmail.com>
2017-05-31 15:10:15 -0400

    init commit of test movie app site with express

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var config = require('../config/config');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	const apiBaseUrl = 'http://api.themoviedb.org/3';
+	const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key='+config.apiKey;
+	const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';	
+	request.get(nowPlayingUrl,(error,response,movieData)=>{
+		var movieData = JSON.parse(movieData);
+  		res.render('index', { 
+  			movieData: movieData.results,
+  			imageBaseUrl : imageBaseUrl
+  		});
+	});
+
+});
+
+module.exports = router;
