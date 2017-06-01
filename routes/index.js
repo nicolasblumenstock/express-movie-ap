77ad68213ce02b81884782d678e1fd02f74c7681
Nicolas Blumenstock <nicpblum@gmail.com>
2017-06-01 15:47:23 -0400

    modified to add search and single movie selection pages

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,47 @@ var router = express.Router();
 var request = require('request');
 var config = require('../config/config');
 
+const apiBaseUrl = 'http://api.themoviedb.org/3';
+const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key='+config.apiKey;
+const imageBaseUrl = 'http://image.tmdb.org/t/p';	
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	const apiBaseUrl = 'http://api.themoviedb.org/3';
-	const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key='+config.apiKey;
-	const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';	
+
 	request.get(nowPlayingUrl,(error,response,movieData)=>{
 		var movieData = JSON.parse(movieData);
   		res.render('index', { 
   			movieData: movieData.results,
-  			imageBaseUrl : imageBaseUrl
+  			imageBaseUrl : `${imageBaseUrl}/w300`,
+  			title: 'Welcome to the Thunderdome. These are Now in Theaters'
   		});
 	});
 
+});
+
+router.get('/movie/:id',(req,res)=>{
+	var thisMovieId = req.params.id
+	var thisMovieUrl = `${apiBaseUrl}/movie/${thisMovieId}?api_key=${config.apiKey}`;	
+	request.get(thisMovieUrl, (error,response,movieData)=>{
+		var movieData = JSON.parse(movieData);
+		res.render('singleMovie',{
+			movieData: movieData,
+			imageBaseUrl: `${imageBaseUrl}/w600`,
+		})
+	});
+});
+
+router.post('/search',(req,res)=>{
+	var termUserSearchedFor = req.body.searchString;
+	var searchUrl = apiBaseUrl + '/search/movie?query='+termUserSearchedFor+'&api_key='+config.apiKey;
+	request.get(searchUrl, (error,response,movieData)=>{
+		var movieData = JSON.parse(movieData);
+		res.render('index',{
+			movieData: movieData.results,
+			imageBaseUrl: `${imageBaseUrl}/w300`,
+			title: `Ooo. Look at all the ${termUserSearchedFor} ...`
+		})
+	})
 });
 
 module.exports = router;
